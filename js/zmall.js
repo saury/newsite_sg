a399053a6aeb30807e97b4eadfaf91dcac75946e
Jin_mbp <erlking@qq.com>
2015-11-20 18:00:35 +0800

    admin pages added

--- js/zmall.js
+++ js/zmall.js
@@ -77,7 +77,12 @@ $(function() {
             //hide the "You have xx more items" when the num is 0
             if (overflow) var _overflowStr = '<div class="mini-cart-hint cf"><p class="fll">You have <em>' + overflow + '</em> more items</p><p class="flr">Total: <em>S$234.23</em></p></div>';
             else _overflowStr = '';
-            var _item = '<li class="mini-cart-item pure-g"><div class="pure-u-1-6"><img src="img/brands-thumb.jpg" class="img-responsive"></div><div class="pure-u-7-12"><p>Content content content</p><span>color: 1 Size: M</span></div><div class="pure-u-1-4 text-right"><em>S$234.56</em><a href="#" class="btn btn-warning hollow btn-xs">Delete</a></div><li><li class="mini-cart-item pure-g"><div class="pure-u-1-6"><img src="img/brands-thumb.jpg" class="img-responsive"></div><div class="pure-u-7-12"><p>Content content content</p><span>color: 1 Size: M</span></div><div class="pure-u-1-4 text-right"><em>S$234.56</em><a href="#" class="btn btn-warning hollow btn-xs">Delete</a></div><li><li class="mini-cart-item pure-g"><div class="pure-u-1-6"><img src="img/brands-thumb.jpg" class="img-responsive"></div><div class="pure-u-7-12"><p>Content content content</p><span>color: 1 Size: M</span></div><div class="pure-u-1-4 text-right"><em>S$234.56</em><a href="#" class="btn btn-warning hollow btn-xs">Delete</a></div><li>';
+            var _item=''; 
+            $.each(item, function(i,data) {
+                 _item +='<li class="mini-cart-item pure-g"><div class="pure-u-1-6"><img src="'+data.cart_p_image+'" class="img-responsive"></div><div class="pure-u-7-12"><p>'+data.cart_p_name+'</p><span>'+data.cart_p_attr+'</span></div><div class="pure-u-1-4 text-right"><em>'+data.cart_p_exchange_price+'</em><a href="#" class="btn btn-warning hollow btn-xs cart_delete" cart_id="'+data.cart_id+'">Delete</a></div><li>';
+            });
+            
+            //var _item = '<li class="mini-cart-item pure-g"><div class="pure-u-1-6"><img src="img/brands-thumb.jpg" class="img-responsive"></div><div class="pure-u-7-12"><p>Content content content</p><span>color: 1 Size: M</span></div><div class="pure-u-1-4 text-right"><em>S$234.56</em><a href="#" class="btn btn-warning hollow btn-xs">Delete</a></div><li>';
             // for (var i = 0; i < item.length; i++) {
             //     _item += "<li>"+item[i]+"</li>";
             // }
@@ -87,7 +92,7 @@ $(function() {
                 _item +
                 '</ul>' +
                 _overflowStr +
-                '<a class="btn btn-warning btn-sm flr" href="#">Checking my cart</a>';
+                '<a class="btn btn-warning btn-sm flr" href="/shoppingcart/cart">Checking my cart</a>';
         };
 
         //function for the floor jumping
@@ -118,8 +123,10 @@ $(function() {
     $('[data-toggle="popover"]').each(function() {
         var element = $(this);
         var tiktak = function() {};
-        var item = ["aaa", "bbb", "ccc", "ddd", "eee", "fff"]; //商品内容
-        var overflow = item.length > 5 ? (item.length - 5) : 0; //超出商品显示限制条数
+//        console.log(item);
+//        console.log(shopcart_no);
+        //var item = ["aaa", "bbb", "ccc", "ddd", "eee", "fff"]; //商品内容
+        var overflow = shopcart_no > 5 ? (shopcart_no - 5) : 0; //超出商品显示限制条数
         element.on("mouseenter", function() {
             element.popover({
                 trigger: 'manual',
@@ -134,8 +141,8 @@ $(function() {
                 element.popover('destroy');
             });
         }).on("mouseleave", function() {
-            item = ["aaa", "bbb", "ccc", "ddd", "eee"];
-            overflow = item.length > 5 ? (item.length - 5) : 0;
+           //item = ["aaa", "bbb", "ccc", "ddd", "eee"];
+            overflow = shopcart_no > 5 ? (shopcart_no - 5) : 0;
             tiktak = setTimeout(function() {
                 if (!$(".popover:hover").length) {
                     //element.popover("hide");
@@ -220,25 +227,39 @@ $(function() {
         element.attr("src", src_cur);
     });
     //active toggle
-    $('[active-toggle="true"]').each(function() {
-        var ul_ele = $(this);
-        ul_ele.children("li").each(function() {
-            var li_ele = $(this);
-            li_ele.on("click", function() {
-                li_ele.toggleClass("active");
-                li_ele.find("i").toggleClass("glyphicon-ok");
-            });
-        });
-    });
+    // $('[active-toggle="true"]').each(function() {
+    //     var ul_ele = $(this);
+    //     ul_ele.children("li").each(function() {
+    //         var li_ele = $(this);
+    //         li_ele.on("click", function() {
+    //             li_ele.toggleClass("active");
+    //             li_ele.find("i").toggleClass("glyphicon-ok");
+    //         });
+    //     });
+    // });
     //call Affix Overflow
     zmModual.callAOF();
     // gallary photo override
+    // $("[gallary-override]").each(function() {
+    //     var ele = $(this),
+    //         target = $(".detail-gallary-override");
+    //     var arg = ele.attr("gallary-override");
+    //     ele.on("click", function() {
+    //         if (arg == "true") target.show();
+    //         else target.hide();
+    //     });
+    // });
+        // src pass added
     $("[gallary-override]").each(function() {
         var ele = $(this),
             target = $(".detail-gallary-override");
-        var arg = ele.attr("gallary-override");
+        var arg = ele.attr("gallary-override"),//override the gallary photo or not
+            src_400 = ele.children("img").attr("src");
         ele.on("click", function() {
-            if (arg == "true") target.show();
+            if (arg == "true") {
+                target.children("img").attr("src",src_400.replace("40x40","400x400"));
+                target.show();
+            }
             else target.hide();
         });
     });
